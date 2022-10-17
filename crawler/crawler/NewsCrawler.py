4dc1065031a951acd4a535929f50ab2dde4d58bd
leebyeounggwan <gwan0622@gmail.com>
2022-10-17 23:44:15 +0900

    add : 뉴스 크롤러 생성 및 데이터베이스 연결

--- crawler/crawler/NewsCrawler.py
+++ crawler/crawler/NewsCrawler.py
@@ -1,14 +1,38 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import MySQLdb
+import mysql_auth
+info = mysql_auth.info
 
+conn = MySQLdb.connect(
+    user=info['user'],
+    passwd=info['passwd'],
+    host=info['host'],
+    db=info['db']
+)
+
+print(type(conn))
+cursor = conn.cursor()
+print(type(cursor))
 
 html = urlopen("https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=%EB%86%8D%EC%97%85")
 soup = BeautifulSoup(html, "html.parser")
 
-newsList = soup.select(".news_area")
-imageList = soup.select(".dsc_thumb")
+result = soup.select_one(".list_news").select(".bx")
+count = len(result)
 
-for i in range(len(newsList)):
+newsList = list()
+imageList = list()
+
+for i in range(count):
+    newsList.append(result[i].select_one(".news_area"))
+    if result[i].select_one(".dsc_thumb").find("img")["src"] is None:
+        imageList.append('empty')
+    else:
+        imageList.append(result[i].select_one(".dsc_thumb").find("img")["src"])
+
+
+for i in range(count):
     pressNameCheck = newsList[i].select_one(".news_info").select_one(".info_group").select_one(".info").i
 
     if pressNameCheck is not None:
@@ -18,7 +42,16 @@ for i in range(len(newsList)):
     title = newsList[i].select_one(".news_tit").text
     url = newsList[i].select_one(".news_tit").attrs["href"]
     press = newsList[i].select_one(".news_info").select_one(".info_group").select_one(".info").text
-    img = imageList[i].find("img")["src"]
+    img = imageList[i]
+    sql = (
+        "INSERT INTO sys.news2 (time,title,url,press,img) "
+        "VALUES (%s, %s, %s, %s, %s)"
+    )
+    data = (time, title, url, press, img)
+    cursor.execute(sql, data)
+    # print(time, title, url, press, img)
+conn.commit()
+conn.close()
 
 
 
--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -9,24 +9,38 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os, json
 from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w4#8ulx5#yf#n7%m@@nkvtv@8bbp%0j6ya^#py)&#xyil4oy2r'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+
+SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'crawler.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -80,7 +93,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -100,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -111,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
